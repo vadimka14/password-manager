ac5d6808a709489dca6a94c1e243e571e75b7720
vadimka14 <vadimgolovach2017@gmail.com>
2026-08-28 23:35:29 +0200

    json

--- main.go
+++ main.go
@@ -23,6 +23,23 @@ func NewPassword(name, value, category string) Password {
 	}
 }
 
+type PasswordManager struct {
+	passwords     map[string]Password `json:"passwords"`
+	masterKey     []byte              `json:"-"`
+	filePath      string              `json:"-"`
+	isInitialized bool                `json:"-"`
+}
+
+func NewPasswordManager(filepath string) *PasswordManager {
+	passwords := make(map[string]Password)
+	return &PasswordManager{
+		passwords:     passwords,
+		masterKey:     nil,
+		filePath:      filepath,
+		isInitialized: false,
+	}
+}
+
 func main() {
 	// password := NewPassword("anten41k", "39f93fffj9dfd", "kaba4ki")
 	// data, err := json.Marshal(password)
