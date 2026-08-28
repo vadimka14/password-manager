4930b5c3f2e6c57acbdd2f62e0fa1e1f4c4c8d97
vadimka14 <vadimgolovach2017@gmail.com>
2026-08-28 22:51:56 +0200

    json

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Password struct {
+	Name         string `json:"name"`
+	Value        string `json:"value"`
+	Category     string `json:"category"`
+	CreatedAt    time.Time
+	LastModified time.Time
+}
+
+func NewPassword(name, value, category string) Password {
+
+	return Password{
+		Name:         name,
+		Value:        value,
+		Category:     category,
+		CreatedAt:    time.Now(),
+		LastModified: time.Now(),
+	}
+}
+
 func main() {
+	password := NewPassword("anten41k", "39f93fffj9dfd", "kaba4ki")
+	data, err := json.Marshal(password)
+	if err != nil {
+		fmt.Errorf("encode password: %w", err)
+	}
+	fmt.Println(string(data))
 
 }
